2fe7bb5c1e8fa5c11e3c3da637c91a260d3b1d54
Lin <weihaolin2008@qq.com>
2016-05-25 14:05:21 +0800

    Array/数组的操作

--- Array/Array.java
+++ Array/Array.java
@@ -1,5 +1,10 @@
 package Array;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class Array {
 	/**
 	 * 寻找数组中的最小值和最大值
@@ -37,6 +42,86 @@ public class Array {
 			}
 		}
 		return new MaxMin(Max,Min);
+	}
+	
+	/**
+	 * 找出数组中第二大的数
+	 * @param a
+	 * @return
+	 */
+	public static int findSecMax(int[] a){
+		int len = a.length;
+		int max = a[0];
+		int sec_max = Integer.MIN_VALUE;
+		for(int i = 0; i < len; i++){
+			if(a[i] > max){
+				sec_max = max;
+				max = a[i];
+			}else{
+				if(a[i] > sec_max){
+					sec_max = a[i];
+				}
+			}
+		}
+		return sec_max;
+	}
+	
+	/**
+	 * 问题描述：找出数组中重复元素最多的数
+	 * @param a
+	 * @return
+	 */
+	public static int findMostFrequnentInArray(int[] a){
+		int result = 0;
+		int size = a.length;
+		if(size == 0){
+			return Integer.MAX_VALUE;
+		}
+		
+		//记录每个元素出现的次数
+		Map<Integer,Integer> m = new HashMap<Integer,Integer>();
+		for(int i = 0; i < size; i++){
+			if(m.containsKey(a[i])){
+				m.put(a[i], m.get(a[i]) + 1);
+			}else{
+				m.put(a[i], 1);
+			}
+		}
+		
+		//用迭代器找出出现次数最多的元素
+		int most = 0;
+		Iterator it = m.entrySet().iterator();
+		while(it.hasNext()){
+			Map.Entry entry = (Map.Entry) it.next();
+			int key = (int) entry.getKey();
+			int value = (int) entry.getValue();
+			if(value > most){
+				result = key;
+				most = value;
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * 问题描述：给定数组a={3,4,5,6,5,6,7,8}，这个数组中相邻元素之差为1，
+	 *        给定数字9，找出它在数组中第一次出现的下标
+	 * 解决方案：跳跃搜索法(加速遍历)
+	 * @param a  给定数组
+	 * @param t  给定数字
+	 * @return
+	 */
+	public static int findIndex(int[] a, int t){
+		int len = a.length;
+		int i = 0;
+		while(i < len){
+			if(a[i] == t){
+				return i;
+			}else{
+				i += Math.abs(t - a[i]);
+			}
+		}
+		return -1;
 	}
 
 }
