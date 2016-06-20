f394da3d3631cd21701720bf37ee7c5a09bf6a49
Lin <weihaolin2008@qq.com>
2016-06-20 18:28:33 +0800

    LinkedList/单链表的实现及更多操作

--- LinkedList/Test.java
+++ LinkedList/Test.java
@@ -1,23 +1,54 @@
 package LinkedList;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
 public class Test {
 
 	//≤‚ ‘
 	public static void main(String[] args) {
-		MyLinkedList list = new MyLinkedList();
-		list.addNode(5);
-		list.addNode(3);
-		list.addNode(1);
-		list.addNode(3);
-		OperateList.deletDuplecate1(list.head);
-		System.out.println("listLen= " + list.length());
-		System.out.println("before sort:");
-		list.printList();
-		list.orderList();
-		System.out.println("after sort:");
-		list.printList();
+//		MyLinkedList list = new MyLinkedList();
+//		list.addNode(5);
+//		list.addNode(3);
+//		list.addNode(1);
+//		list.addNode(3);
+//		OperateList.deletDuplecate1(list.head);
+//		System.out.println("listLen= " + list.length());
+//		System.out.println("before sort:");
+//		list.printList();
+//		list.orderList();
+//		System.out.println("after sort:");
+//		list.printList();
+//        DLinkedList<Integer> lst = new DLinkedList<>( );
+//        List<Integer> lt = new ArrayList<>( );
+//        for( int i = 0; i < 10; i++ )
+//                lst.add( i );
+//        for( int i = 20; i < 30; i++ )
+//                lst.add( 0, i );
+//
+//        lst.remove( 0 );
+//        lst.remove( lst.size( ) - 1 );
+//
+//        System.out.println( lst );
+//
+//        java.util.Iterator<Integer> itr = lst.iterator( ); 
+//        while( itr.hasNext( ) )
+//        {
+//                itr.next( );
+//                itr.remove( );
+//                System.out.println( lst );
+//        }
 
-
+        Map<Integer, Integer> m = new ConcurrentHashMap<>();
+        String s = "adas";
+        if(s.contains("adsa")){
+        	System.out.println("aa");
+        }
 
 	}
 
